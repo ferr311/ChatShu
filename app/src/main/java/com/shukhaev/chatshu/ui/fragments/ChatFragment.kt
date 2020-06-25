ac6599d3732256529f8ed8b2102da54d3c73d322
Konstantin <ferr311@mail.ru>
2020-06-25 17:29:17 +0300

    add SettingsFragment.kt

---
+++ app/src/main/java/com/shukhaev/chatshu/ui/fragments/ChatFragment.kt
@@ -0,0 +1,14 @@
+package com.shukhaev.chatshu.ui.fragments
+
+import com.shukhaev.chatshu.R
+
+
+class ChatFragment : BaseFragment(R.layout.fragment_chat) {
+
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+}
